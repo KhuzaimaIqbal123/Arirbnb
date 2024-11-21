56880118176ce9d11f15515545f3a0f129480dd0
Khuzaima <114830749+KhuzaimaIqbal123@users.noreply.github.com>
2024-11-21 13:16:21 +0500

    ListingCard Component and Footer component rendered with theri CSS's

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -2,15 +2,32 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import SearchBar from "../Components/Searchbar";
 import Categories from "../Components/Categories";
+import ListingCard from "../Components/ListingCard";
+import Footer from "../Components/Footer";
 
-const App= ()=>{
-return (
-<div>
-  <Navbar/>
-  <SearchBar/>
-  <Categories/>
+function App() {
+  const property = {
+      image: 'https://via.placeholder.com/300',
+      title: 'Beautiful Beach House',
+      type: 'Entire home',
+      guests: 4,
+      bedrooms: 2,
+      bathrooms: 1,
+      price: 150,
+      rating: 4.8
+  };
 
-</div>
-)
-};
+  return (
+      <div>
+          <Navbar />
+          <SearchBar />
+          <Categories />
+          <div className="App">
+              <ListingCard property={property} />
+          </div>
+          <Footer />
+      </div>
+  );
+}
+
 export default App;
