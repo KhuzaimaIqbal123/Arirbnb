a3f65cdff0f0123717f24f21b36e85e2b4f003e4
Khuzaima <114830749+KhuzaimaIqbal123@users.noreply.github.com>
2024-11-21 15:26:05 +0500

    Images updated in Listing Cards

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   // Array of property objects
   const properties = [
     {
-      image: "https://via.placeholder.com/300",
+      image: "https://a0.muscache.com/im/pictures/miso/Hosting-591171879509364369/original/1931fbae-0d17-4ef8-9cbe-595c7a29ee73.jpeg?im_w=720",
       title: "Beautiful Beach House",
       type: "Entire home",
       guests: 4,
@@ -20,7 +20,7 @@ function App() {
       rating: 4.8,
     },
     {
-      image: "https://via.placeholder.com/300",
+      image: "https://a0.muscache.com/im/pictures/miso/Hosting-34387720/original/be3eb0b9-0148-43da-9eb7-d5d8d3443a3e.jpeg?im_w=720",
       title: "Cozy Mountain Cabin",
       type: "Cabin",
       guests: 6,
@@ -30,7 +30,7 @@ function App() {
       rating: 4.9,
     },
     {
-      image: "https://via.placeholder.com/300",
+      image: "https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTI4NzU3MTgwMzg2NDk2OTQxMw%3D%3D/original/02ea369e-50f3-4461-a68c-3556cb35aff7.jpeg?im_w=720",
       title: "Luxury City Apartment",
       type: "Apartment",
       guests: 2,
@@ -40,7 +40,7 @@ function App() {
       rating: 4.7,
     },
     {
-      image: "https://via.placeholder.com/300",
+      image: "https://a0.muscache.com/im/pictures/hosting/Hosting-1161447484734690123/original/cbd576cc-3e2c-4318-bc28-d324fd1fdb7f.jpeg?im_w=720",
       title: "Luxury City Apartment",
       type: "Apartment",
       guests: 2,
@@ -50,7 +50,7 @@ function App() {
       rating: 4.7,
     },
     {
-      image: "https://via.placeholder.com/300",
+      image: "https://a0.muscache.com/im/pictures/miso/Hosting-746919020186561409/original/f4199828-cbf8-44b0-a29b-0bed8ffc989f.jpeg?im_w=720",
       title: "Luxury City Apartment",
       type: "Apartment",
       guests: 2,
@@ -60,7 +60,7 @@ function App() {
       rating: 4.7,
     },
     {
-      image: "https://via.placeholder.com/300",
+      image: "https://a0.muscache.com/im/pictures/miso/Hosting-636650621122218401/original/dddd42d4-3e19-4bde-bbb8-a086d4534f8e.jpeg?im_w=720",
       title: "Luxury City Apartment",
       type: "Apartment",
       guests: 2,
@@ -70,7 +70,7 @@ function App() {
       rating: 4.7,
     },
     {
-      image: "https://via.placeholder.com/300",
+      image: "https://a0.muscache.com/im/pictures/miso/Hosting-8525016/original/675c507e-116e-4da7-8c32-976e704cb617.jpeg?im_w=720",
       title: "Luxury City Apartment",
       type: "Apartment",
       guests: 2,
@@ -79,7 +79,7 @@ function App() {
       price: 300,
       rating: 4.7,
     }, {
-      image: "https://via.placeholder.com/300",
+      image: "https://a0.muscache.com/im/pictures/airflow/Hosting-21823836/original/87669a54-58c8-4682-a41a-05581fff7ec6.jpg?im_w=720",
       title: "Luxury City Apartment",
       type: "Apartment",
       guests: 2,
@@ -88,7 +88,7 @@ function App() {
       price: 300,
       rating: 4.7,
     }, {
-      image: "https://via.placeholder.com/300",
+      image: "https://a0.muscache.com/im/pictures/5a3af603-e470-44ce-ae09-ee67aa1c7614.jpg?im_w=720",
       title: "Luxury City Apartment",
       type: "Apartment",
       guests: 2,
@@ -97,7 +97,7 @@ function App() {
       price: 300,
       rating: 4.7,
     }, {
-      image: "https://via.placeholder.com/300",
+      image: "https://a0.muscache.com/im/pictures/896289f9-341b-4614-867c-5d8e910e5080.jpg?im_w=720",
       title: "Luxury City Apartment",
       type: "Apartment",
       guests: 2,
